929ce8f477b6b6846f3e9332075ab85100a5387e
sdpatro <sidharth.patro@outlook.com>
2016-05-10 02:12:36 +0530

    Initial Commit #2

--- Radient_Django/urls.py
+++ Radient_Django/urls.py
@@ -5,7 +5,6 @@ Definition of urls for Radient_Django.
 from datetime import datetime
 from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
-from Radient_Django import settings
 
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
@@ -15,6 +14,26 @@ from Radient_Django import settings
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'app.views.home', name='home'),
+    url(r'^contact$', 'app.views.contact', name='contact'),
+    url(r'^about', 'app.views.about', name='about'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -22,4 +41,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
-
--- app/tests.py
+++ app/tests.py
@@ -17,3 +17,18 @@ class ViewTest(TestCase):
         def setUpClass(cls):
             super(ViewTest, cls).setUpClass()
             django.setup()
+
+    def test_home(self):
+        """Tests the home page."""
+        response = self.client.get('/')
+        self.assertContains(response, 'Home Page', 1, 200)
+
+    def test_contact(self):
+        """Tests the contact page."""
+        response = self.client.get('/contact')
+        self.assertContains(response, 'Contact', 3, 200)
+
+    def test_about(self):
+        """Tests the about page."""
+        response = self.client.get('/about')
+        self.assertContains(response, 'About', 3, 200)
